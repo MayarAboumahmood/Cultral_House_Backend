98765169d257b120a81748ca14a53249a13277f5
Ghassan <johndoe@example.com>
2023-05-19 20:50:36 +0300

    admin deletion is now working for the super admin

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,19 +1,19 @@
 //importing modules
 const express = require('express')
 const adminController = require('../controllers/AdminController')
-const {createAdmin, login,deleteAdmin} = adminController
+const {createAdmin, login, deleteAdmin} = adminController
 const checkAuth = require('../middleware/checkAuth')
 
 const router = express.Router()
 
 //signup endpoint
 //passing the middleware function to the signup
-router.post('/signup', checkAuth.checkIfSuper,checkAuth.saveAdmin, createAdmin)
+router.post('/signup', checkAuth.checkIfSuper, checkAuth.saveAdmin, createAdmin)
 
 //login route
 router.post('/login', login)
 
-router.delete('/delete',checkAuth.checkIfSuper,deleteAdmin)
+router.delete('/delete', checkAuth.checkIfSuper, deleteAdmin)
 
 
 module.exports = router
